cc7121db413f58da444e9b59d70b5ad06a0a00dc
Célio <celiofagundes30@gmail.com>
2022-03-22 14:56:04 -0300

    init commit

--- controllers/pages.js
+++ controllers/pages.js
@@ -9,7 +9,7 @@ const calc = (req, res) => {
   const num1 = parseInt(req.params.num1)
   const num2 = parseInt(req.params.num2)
   if (num2 === 0) {
-    res.send('DivisaoZero')
+    res.send('DivisaoZeros')
   } else {
     if (num1 % num2 === 0) {
       res.send('Divisivel')
